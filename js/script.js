3f6831efcb93c85fd68d9b8a61c91ae917b6d9bc
cornelco <codysmart@gmail.com>
2018-01-25 00:34:16 -0800

    Adding the rest of the silly WebStorm files.

--- js/script.js
+++ js/script.js
@@ -4,9 +4,9 @@ document.addEventListener('init', function(event) {
 
     if (page.id === 'myLists') {
         page.querySelector('#push-button').onclick = function() {
-            document.querySelector('#myNavigator').pushPage('tab2.html', {data: {title: 'hi'}});
+            document.querySelector('#myNavigator').pushPage('listItems.html', {data: {title: 'sup'}});
         };
-    } else if (page.id === 'tab2') {
+    } else if (page.id === 'listItems') {
         page.querySelector('ons-toolbar .center').innerHTML = page.data.title;
     }
 });
