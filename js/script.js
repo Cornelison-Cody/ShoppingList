62b29c502d7210629c8dc4b068269325278b0330
cornelco <codysmart@gmail.com>
2018-01-29 00:52:35 -0800

    Started working on App. Got a fair bit of the front end done. No backend is connected yet.

--- js/script.js
+++ js/script.js
@@ -2,11 +2,21 @@ document.addEventListener('init', function(event) {
     var page = event.target;
     console.log("Page: ", page.id);
 
-    if (page.id === 'myLists') {
-        page.querySelector('#push-button').onclick = function() {
-            document.querySelector('#myNavigator').pushPage('listItems.html', {data: {title: 'sup'}});
+    if (page.id === 'mainList') {
+        page.querySelector('#essentials').onclick = function() {
+            document.querySelector('#myNavigator').pushPage('essentials.html');
         };
-    } else if (page.id === 'listItems') {
-        page.querySelector('ons-toolbar .center').innerHTML = page.data.title;
+        page.querySelector('#wants').onclick = function() {
+            document.querySelector('#myNavigator').pushPage('wants.html');
+        };
+        page.querySelector('#needs').onclick = function() {
+            document.querySelector('#myNavigator').pushPage('needs.html');
+        };
+        page.querySelector('#dinners').onclick = function() {
+            document.querySelector('#myNavigator').pushPage('dinners.html');
+        };
+        page.querySelector('#shoppingList').onclick = function() {
+            document.querySelector('#myNavigator').pushPage('shoppingList.html');
+        };
     }
 });
